0ca7217d093f2bf8083542cbabbee384bf0ff303
smr10110 <a.huaiquil02@ufromail.cl>
2024-04-18 09:29:34 -0400

    Segundo commit.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final int DIAS = 7;
+    private static final int HORAS = 24;
+    private static double[][] sismos = new double[DIAS][HORAS];
+    private static Scanner scanner = new Scanner(System.in);
+    private double[][] generarDatosSismicos(int dias) {
+        double[][] lecturasSismicas = new double[dias][24];
+        Random random = new Random();
+        for (int dia = 0; dia < dias; dia++) {
+            for (int hora = 0; hora < 24; hora++) {
+                lecturasSismicas[dia][hora] = 1.0 + (9.5 - 1.0) * random.nextDouble();
+            }
+        }
+        return lecturasSismicas;
+    }
+
+    private void encontrarYMostrarSismoMasIntenso(double[][] lecturasSismicas) {
+        double maxIntensidad = 0;
+        int diaDelMaximo = -1;
+        int horaDelMaximo = -1;
+        for (int dia = 0; dia < lecturasSismicas.length; dia++) {
+            for (int hora = 0; hora < lecturasSismicas[dia].length; hora++) {
+                if (lecturasSismicas[dia][hora] > maxIntensidad) {
+                    maxIntensidad = lecturasSismicas[dia][hora];
+                    diaDelMaximo = dia;
+                    horaDelMaximo = hora;
+                }
+            }
+        }
+        System.out.printf("El día %d, a las %02d:00, se registró el sismo más intenso, con una magnitud de %.2f en la escala de Richter.\n",
+                diaDelMaximo + 1, horaDelMaximo, maxIntensidad);
     }
 }
